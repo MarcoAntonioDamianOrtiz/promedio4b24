c2e6d8b64f6aa2a489b7859ffd92b92fc5078509
Marco.Antonio <mo27marco@gmail.com>
2024-09-17 22:35:10 -0500

    Commit inicial

--- test 2/perfectos.py
+++ test 2/perfectos.py
@@ -1,3 +1,6 @@
 def numero_perfecto(n):
-    suma_divisores = sum([i for i in range(1, n) if n % i == 0])
+    if n < 2:
+        return False
+
+    suma_divisores = sum(i for i in range(1, n) if n % i == 0)
     return suma_divisores == n
--- test 2/test_perfectos.py
+++ test 2/test_perfectos.py
@@ -3,6 +3,11 @@ from perfectos import numero_perfecto
 
 class TestNumeroPerfecto(unittest.TestCase):
     def test_es_numero_perfecto(self):
-        self.assertTrue(numero_perfecto(6))  
-if __name__ == '__main__':
-    unittest.main()
+        # Pruebas para números perfectos
+        self.assertTrue(numero_perfecto(6))   # 6 es un número perfecto
+        self.assertTrue(numero_perfecto(28))  # 28 es un número perfecto
+
+    def test_no_es_numero_perfecto(self):
+        # Pruebas para números que no son perfectos
+        self.assertFalse(numero_perfecto(10))  # 10 no es un número perfecto
+        self.assertFalse(numero_perfecto(0))    # 0 no es un número
