bd2abb57ad0d54be12fd19c67cab2375a039c0df
Marco.Antonio <mo27marco@gmail.com>
2024-09-17 22:54:29 -0500

    Primer commit

---
+++ test 2/perfectos.py
@@ -0,0 +1,14 @@
+def numero_perfecto(n):
+    print(n)
+    if n < 2:
+        return False
+    
+    suma_divisores = 0
+    for i in range (1, n):
+        if n % i == 0:
+            suma_divisores += i
+            
+        if suma_divisores == n:
+            return True
+        else:
+            return False
